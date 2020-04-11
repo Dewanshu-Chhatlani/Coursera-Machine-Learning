90ca356b7f48b28e03ac187da0c2d545f9fb9daf
Dewanshu-Chhatlani <devanshu.chh@gmail.com>
2020-04-11 17:54:43 +0530

    Completed Gradient Descent

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,11 +17,11 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    hypothesis = X * theta;
+    error = (hypothesis - y);
+    delta = sum(error.*X);
+    theta(1) = theta(1) - alpha * 1/m * delta(1);
+    theta(2) = theta(2) - alpha * 1/m * delta(2);
 
     % ============================================================
 
