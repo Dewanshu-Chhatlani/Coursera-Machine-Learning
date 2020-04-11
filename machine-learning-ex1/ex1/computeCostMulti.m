bc586d4490846aacea12949224d1bf9883227ab2
Dewanshu-Chhatlani <devanshu.chh@gmail.com>
2020-04-11 18:09:34 +0530

    Completed Cost Function for Linear Regression with Multiple Variables

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -14,7 +14,9 @@ J = 0;
 %               You should set J to the cost.
 
 
-
+hypothesis =  X * theta;
+sqrErrors = (hypothesis - y) .^ 2;
+J = 1/(2*m) * sum(sqrErrors);
 
 
 % =========================================================================
