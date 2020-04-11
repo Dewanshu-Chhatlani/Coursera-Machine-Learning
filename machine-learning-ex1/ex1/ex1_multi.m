ec46c50d0b7d44f8c030e1431490bc2e471139d0
Dewanshu-Chhatlani <devanshu.chh@gmail.com>
2020-04-11 18:30:19 +0530

    Estimated the price of a 1650 sq-ft, 3 br house using Normal Equation

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -155,6 +155,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
 
+house_data = [1 1650 3];
+price = house_data * theta;
 
 % ============================================================
 
